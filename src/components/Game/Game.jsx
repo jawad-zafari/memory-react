ef28ba3e8eb90f1ba3f1c5b28b1bd962b276a5ea
jawad zafari <jawad.zafari@laplateforme.io>
2026-05-19 11:59:41 +0200

    la constante cardImages avec les images de cartes

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -14,3 +14,18 @@ import card9 from '../../assets/card9.jpg';
 import card10 from '../../assets/card10.jpg';
 import card11 from '../../assets/card11.jpg';
 import card12 from '../../assets/card12.jpg';
+
+const cardImages = [
+  { id: 1, image: card1 },
+  { id: 2, image: card2 },
+  { id: 3, image: card3 },
+  { id: 4, image: card4 },
+  { id: 5, image: card5 },
+  { id: 6, image: card6 },
+  { id: 7, image: card7 },
+  { id: 8, image: card8 },
+  { id: 9, image: card9 },
+  { id: 10, image: card10 },
+  { id: 11, image: card11 },
+  { id: 12, image: card12 },
+];
