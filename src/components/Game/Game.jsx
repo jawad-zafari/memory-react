476711fb822081697fabf581c921cd7095fb312d
jawad zafari <jawad.zafari@laplateforme.io>
2026-05-19 11:58:26 +0200

    les importations d'images de cartes

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,2 +1,16 @@
 import { useState, useEffect } from 'react';
 import Card from '../Card/Card';
+
+import backImage from '../../assets/back.jpg';
+import card1 from '../../assets/card1.jpg';
+import card2 from '../../assets/card2.jpg';
+import card3 from '../../assets/card3.jpg';
+import card4 from '../../assets/card4.jpg';
+import card5 from '../../assets/card5.jpg';
+import card6 from '../../assets/card6.jpg';
+import card7 from '../../assets/card7.jpg';
+import card8 from '../../assets/card8.jpg';
+import card9 from '../../assets/card9.jpg';
+import card10 from '../../assets/card10.jpg';
+import card11 from '../../assets/card11.jpg';
+import card12 from '../../assets/card12.jpg';
