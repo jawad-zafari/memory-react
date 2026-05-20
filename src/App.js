2f0c611976122d9b13606890df9a18338abc609e
jawad zafari <jawad.zafari@laplateforme.io>
2026-05-20 16:45:12 +0200

    des imports pour les composants Title, Game et Scoreboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { useState } from 'react';
+import Title from './components/Title/Title.jsx';
+import Game from './components/Game/Game.jsx';
+import Scoreboard from './components/Scoreboard/Scoreboard.jsx';
 import './App.css';
 
-function App() {
+const App = () => {
+ 
   return (
     <div className="App">
+     
     </div>
   );
-}
+};
 
 export default App;
